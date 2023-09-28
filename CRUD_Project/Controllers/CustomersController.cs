bcb59be1bcc2f6b2709ee5d739e14328e9b98972
WUPINYING <sheluckys4@gmail.com>
2023-09-28 12:40:56 +0800

    first commit

---
+++ CRUD_Project/Controllers/CustomersController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_Project.Models;
+using CRUD_Project.ViewModel;
+
+namespace CRUD_Project.Controllers
+{
+	public class CustomersController : Controller
+	{
+		private AppDbContext db = new AppDbContext();
+
+		// GET: Customers
+		public ActionResult Index()
+		{
+			//var vm = db.Customers.Select(s => new IndexVM
+			//{
+			//	City = s.City,
+			//	Address = s.Address,
+			//	CompanyName = s.CompanyName,
+			//	ContactName = s.ContactName,
+			//	ContactTitle = s.ContactTitle,
+			//	Country = s.Country,
+			//	CustomerID = s.CustomerID,
+			//	Fax = s.Fax,
+			//	Phone = s.Phone,
+			//	PostalCode = s.PostalCode,
+			//	Region = s.Region
+
+			//}).ToList();
+
+			return View(db.Customers.Select(s => new IndexVM
+			{
+				City = s.City,
+				Address = s.Address,
+				CompanyName = s.CompanyName,
+				ContactName = s.ContactName,
+				ContactTitle = s.ContactTitle,
+				Country = s.Country,
+				CustomerID = s.CustomerID,
+				Fax = s.Fax,
+				Phone = s.Phone,
+				PostalCode = s.PostalCode,
+				Region = s.Region
+
+			}).ToList());
+		}
+
+		// GET: Customers/Details/5
+		public ActionResult Details(string id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Customer customer = db.Customers.Find(id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			return View(customer);
+		}
+
+		// GET: Customers/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Customers/Create
+		// 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+		// 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "CustomerID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax")] Customer customer)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Customers.Add(customer);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(customer);
+		}
+
+		// GET: Customers/Edit/5
+		public ActionResult Edit(string id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Customer customer = db.Customers.Find(id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			return View(customer);
+		}
+
+		// POST: Customers/Edit/5
+		// 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+		// 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "CustomerID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax")] Customer customer)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(customer).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(customer);
+		}
+
+		// GET: Customers/Delete/5
+		public ActionResult Delete(string id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Customer customer = db.Customers.Find(id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			return View(customer);
+		}
+
+		// POST: Customers/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(string id)
+		{
+			Customer customer = db.Customers.Find(id);
+			db.Customers.Remove(customer);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
---
+++ CRUD_Project/Controllers/EmployeesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using CRUD_Project.Models;
+
+namespace CRUD_Project.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Employees
+        public ActionResult Index(int? id)
+        {
+            var employees = db.Employees.Include(e => e.Employee1);
+            ViewBag.ted = id;
+
+            if (id!=null)
+            {
+                employees=employees.Where(e=>e.EmployeeID==id || e.ReportsTo==id);                
+            }
+
+            return View(employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.ReportsTo = new SelectList(db.Employees, "EmployeeID", "LastName");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,LastName,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ReportsTo = new SelectList(db.Employees, "EmployeeID", "LastName", employee.ReportsTo);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ReportsTo = new SelectList(db.Employees, "EmployeeID", "LastName", employee.ReportsTo);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeID,LastName,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ReportsTo = new SelectList(db.Employees, "EmployeeID", "LastName", employee.ReportsTo);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
